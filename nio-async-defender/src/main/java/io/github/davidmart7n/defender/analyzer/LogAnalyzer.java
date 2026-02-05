feb7e1a3d776bde5fd2e4b24278ec3d571500a4f
davidmart7n <davidmartinencuentra@gmail.com>
2026-02-05 21:00:40 +0100

    analyzer started

---
+++ nio-async-defender/src/main/java/io/github/davidmart7n/defender/analyzer/LogAnalyzer.java
@@ -0,0 +1,20 @@
+package io.github.davidmart7n.defender.analyzer;
+
+import java.util.List;
+
+import io.github.davidmart7n.defender.model.LogEntry;
+
+
+public class LogAnalyzer {
+
+    private List<LogEntry> logsList;
+
+    public LogAnalyzer(List<LogEntry> logsList){
+        this.logsList=logsList;
+    }
+
+    public List<LogEntry> securityAnalisis(){
+
+    }
+
+}

505b1629aa7c29bb9d9048458ca72b9301004137
davidmart7n <davidmartinencuentra@gmail.com>
2026-02-06 09:46:28 +0100

    jakarta validation on defenderbrain microservice

--- day-02/defenderbrain/src/main/java/io/github/davidmart7n/defenderbrain/dto/LogEntryDTO.java
+++ day-02/defenderbrain/src/main/java/io/github/davidmart7n/defenderbrain/dto/LogEntryDTO.java
@@ -4,9 +4,23 @@ import java.time.Instant;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.PositiveOrZero;
+
 public record LogEntryDTO(
+    @NotBlank 
     String ip, 
+
+    @NotBlank 
     String path,
+
+    @NotBlank 
     String agent,
+
+    @Min(100) @Max(599) 
     int statusCode
+
 ) {}

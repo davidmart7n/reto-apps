af8d4487d1d50738fb5494f1bd18ce4a21a6513f
davidmart7n <davidmartinencuentra@gmail.com>
2026-02-09 09:00:05 +0100

    Add Home component and env config
    Add top-level .gitignore and mark ai-stack-advisor .env as ignored. Configure application to import optional .env properties Update stack-advisor-ui: add app.html entry, register HomeComponent in the root component, and provide HttpClient and async animations. Implement a standalone HomeComponent using ReactiveForms and PrimeNG inputs that submits project data to AiService.getRecommendation.

--- day-04/stack-advisor-ui/src/app/app.config.ts
+++ day-04/stack-advisor-ui/src/app/app.config.ts
@@ -5,12 +5,16 @@ import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
 import { Theme } from '@primeng/themes';
 import  Aura  from '@primeng/themes/aura'
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(),
+    provideAnimationsAsync(),
     providePrimeNG(
       {theme: {preset : Aura}}
     )
--- day-04/stack-advisor-ui/src/app/home/home.ts
+++ day-04/stack-advisor-ui/src/app/home/home.ts
@@ -1,11 +1,46 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
+import { Button } from 'primeng/button';
+import { Checkbox } from 'primeng/checkbox';
+import { InputText } from 'primeng/inputtext';
+import { AiService } from '../service/ai.service';
+import { Project } from '../model/models'
+
+interface User {
+  name: string;
+  terms: boolean;
+}
 
 @Component({
   selector: 'app-home',
-  imports: [],
-  templateUrl: './home.html',
-  styleUrl: './home.css',
+  standalone: true,
+  imports: [ReactiveFormsModule, InputText, Checkbox, Button],
+  templateUrl: './home.html'
 })
-export class Home {
+export class HomeComponent {
+  loading = false;
+  report = false;
+  projectForm!: FormGroup;
+  project:Project={ projectName: '', projectType:'', teamSkills:'', mainRequirements:''};
 
+  constructor(private fb: FormBuilder, private service: AiService) {
+    this.projectForm = this.fb.group({
+      projectName: [''],
+      projectType: [''],
+      teamSkills: [''],
+      mainRequirements: ['']
+    })
+  }
+
+
+  onSubmit() {
+    if (this.projectForm.valid) {
+      this.loading = true;
+      console.log('Enviando:', this.project);
+      this.project=this.projectForm.value;
+      // Tu API call aquí
+      this.service.getRecommendation(this.project)
+      .subscribe()
+    }
+  }
 }
